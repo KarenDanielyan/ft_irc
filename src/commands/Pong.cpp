425a93e04ffa78d7dfa761e50f4b79b70e61e760
Mariam <mariam@Mariams-MacBook-Pro.local>
2024-12-23 13:31:30 +0400

    done with Ping and Pong

--- src/commands/Pong.cpp
+++ src/commands/Pong.cpp
@@ -1,4 +1,4 @@
-#include "../include/Command.hpp"
+#include "Command.hpp"
 
 Pong::Pong(Server* server): Command(server)
 {
@@ -10,11 +10,16 @@ Pong::~Pong()
 
 void Pong::implement(IRCClient* client, std::vector<std::string> arg)
 {
-	(void)client;
-	(void)arg;
-	if (arg.empty()) {
+	if (arg.empty())
+	{
 		throw ReplyException(ERR_NEEDMOREPARAMS("PONG"));
 		return;
 	}
-	throw ReplyException(arg[0]); // or somehow to server
+	std::string message = "";
+	for (unsigned long i = 1; i < arg.size(); i++)
+	{
+		message += arg[i];
+		message += " ";
+	}
+	throw ReplyException(RPL_PING(message));
 }
