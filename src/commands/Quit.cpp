a755aa8d53bd6495805b007eb3afeecb51341b8d
Mariam <mariam@Mariams-MacBook-Pro.local>
2024-12-23 13:05:19 +0400

    done with Quit

--- src/commands/Quit.cpp
+++ src/commands/Quit.cpp
@@ -1,5 +1,5 @@
 #include "../include/Command.hpp"
-//with Karen
+
 Quit::Quit(Server* server): Command(server)
 {
 }
@@ -10,12 +10,9 @@ Quit::~Quit()
 
 void Quit::implement(IRCClient* client, std::vector<std::string> arg)
 {
-	(void)client;
-	(void)arg;
-	// std::string reason;
-	// if (arg[0][0] == ":")
-	// 	reason = arg[0].substr(1);
-	// else
-	// 	reason = "quit with no reason";
-	//h
+	if (arg[0][0] == ":")
+		reason = arg[0].substr(1);
+	else
+		reason = "quit with no reason";
+	SendMessage(client, ":" + reason);
 }
