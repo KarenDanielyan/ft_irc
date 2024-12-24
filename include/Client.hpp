3971060c9b9962459a78f5c5c7f34b1efcbe1cb4
Karen Danielyan <karendanielyan66@gmail.com>
2024-12-24 17:56:22 +0400

    Encapsulation for client data fields

--- include/Client.hpp
+++ include/Client.hpp
@@ -6,7 +6,7 @@
 /*   By: kdaniely <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 15:13:12 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/12/24 17:07:25 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/12/24 17:18:04 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,39 @@
 # include "Connection.hpp"
 # include "Channel.hpp"
 
-class	Client : public Connection
+class	Client
 {
-private:
-	std::string	_nickname;
-	std::string	_username;
-	std::string	_realname;
-	Channel		*_channel;
 public:
-	enum	e_ClientState
+	enum	ClientState
 	{
 		LOGIN,
 		LIVE,
 		DISCONNECTED
 	};
-	Client(int fd, char *hostname, unsigned short port);
+private:
+	std::string			_nickname;
+	std::string			_username;
+	std::string			_realname;
 
+	ClientState			_state;
+	Channel*			_channel;
+	const Connection*	_connection;
+public:
+	Client(const Connection* connection);
+
+	std::string const &	getNickname(void) const;
+	std::string const &	getUsername(void) const;
+	std::string const &	getRealname(void) const;
+
+	const Channel*		getChannel(void) const;
+	const Connection*	getConnection(void) const;
+
+	void	join(Channel* channel);
+	void	part(void);
+
+	void	setNickname(std::string const & nickname);
+	void	setUsername(std::string const & username);
+	void	setRealname(std::string const & realname);
 	~Client();
 };
 
--- src/application/Client.cpp
+++ src/application/Client.cpp
@@ -13,7 +13,58 @@
 #include "Client.hpp"
 #include "Connection.hpp"
 
-Client::Client(int fd, char *hostname, unsigned short port): \
-	Connection(fd, hostname, port) {}
+Client::Client(const Connection* connection): _state(LOGIN), \
+	_channel(NULL), _connection(connection)
+{}
+
+std::string const &	Client::getNickname(void) const
+{
+	return (_nickname);
+}
+
+std::string const &	Client::getUsername(void) const
+{
+	return (_username);
+}
+
+std::string const &	Client::getRealname(void) const
+{
+	return (_realname);
+}
+
+const Connection*	Client::getConnection(void) const
+{
+	return (_connection);
+}
+
+const Channel*		Client::getChannel(void) const
+{
+	return (_channel);
+}
+
+void	Client::setNickname(std::string const & nickname)
+{
+	_nickname = nickname;
+}
+
+void	Client::setUsername(std::string const & username)
+{
+	_username = username;
+}
+
+void	Client::setRealname(std::string const & realname)
+{
+	_realname = realname;
+}
+
+void	Client::join(Channel* channel)
+{
+	_channel = channel;
+}
+
+void	Client::part(void)
+{
+	_channel = NULL;
+}
 
 Client::~Client(void) {}
