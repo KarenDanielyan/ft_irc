726223d86e6ce74c50336fa5e54914203026da21
Karen Danielyan <karendanielyan66@gmail.com>
2024-12-26 23:41:09 +0400

    HOTFIX: const Connection* instead of Connection*

--- include/network/ITransport.hpp
+++ include/network/ITransport.hpp
@@ -3,27 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ITransport.hpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdaniely <kdaniely@42.fr>                  +#+  +:+       +#+        */
+/*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 14:58:27 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/12/26 14:59:05 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/12/26 21:38:25 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#ifndef ITRANSPORT_HPP
+
+# ifndef ITRANSPORT_HPP
 # define ITRANSPORT_HPP
 
 # include <string>
-# include "Connection.hpp"
+
+class Connection;
 
 class	ITransport
 {
 public:
-	virtual void	reply(Connection* to, std::string const & message) = 0;
+	virtual void	reply(const Connection* to, std::string const & message) = 0;
 	virtual void	broadcast(std::string const & message) = 0;
 
 	virtual ~ITransport() {}
 };
-
-
 
 #endif
