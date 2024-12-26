deaf17b9455a66ab2a70417ddd81b122af056fdb
Liana_Aydinyan <Liana_Aydinyan@kwc-sys.com>
2024-12-26 15:47:28 +0400

    username parsing

--- src/commands/User.cpp
+++ src/commands/User.cpp
@@ -9,37 +9,48 @@ User::~User()
 
 }
 
-
-void User::validate(Client *client, std::vector<std::string> arg) // arg neeed to be changed to IRCSMessage
+void User::validate(Client *client, IRCSMessage message)
 {
-	if ( message._parametrs.size() < 4)
-	{
+	if (message._parameters.size() < 4)
 		throw ReplyException(ERR_NEEDMOREPARAMS("USER"));
-		return ;
-	}
 
 	std::string username = message._parameters[0];
 	std::string param2 = message._parameters[1];
 	std::string param3 = message._parameters[2];
 	std::string realname = message._parameters[3];
+
 	if (username.empty())
-	{
 		throw ReplyException(ERR_NEEDMOREPARAMS("USER"));
-		return ; //// ??????
+
+
+	if (!username.empty() && username[0] == ' ')
+		username.erase(0, 1);
+
+	std::string user;
+	if (!username.empty())
+	{
+		size_t space_pos = msg.find(' ');
+		user = username.substr(0, space_pos != std::string::npos ? space_pos : username.size());
 	}
+	username = user;
 	if (param2 != "0" || param3 != "*")
-	{
 		throw ReplyException(ERR_NEEDMOREPARAMS("USER"));
-		return;
-	}
-	else if (realname.empty() || realname[0] != ':')
+
+	if (realname.empty())
+		realname = "";
+
+	size_t start_pos = 0;
+	for (int i = 0; i < 3; ++i)
 	{
-		throw ReplyException(ERR_NEEDMOREPARAMS("USER"));
-		return;
+		start_pos = realname.find(' ', start_pos + 1);
+		if (start_pos == std::string::npos)
+			realname = "";
 	}
 
-	// last paramter should be the realname
-	// what is meaningfull???
+	std::string real = realname.substr(start_pos + 1);
+	if (!real.empty() && real[0] == ':')
+		real.erase(0, 1); // Remove the colon
+
 }
 
 void User::implement(Client *client, std::vector<std::string> arg ,ITransport* server, \
