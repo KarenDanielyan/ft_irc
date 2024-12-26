8c85f25e138ec832d76a8a6aedca1a081e18f0a5
Karen Danielyan <karendanielyan66@gmail.com>
2024-12-27 00:34:42 +0400

    Implementation of a generic multi-cast group

---
+++ src/commands/IChannelCommand.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Command.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/27 00:15:40 by kdaniely          #+#    #+#             */
+/*   Updated: 2024/12/27 00:19:46 by kdaniely         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Command.hpp"
+#include "Channel.hpp"
+
+void IChannelCommand::broadcast(ITransport* server, Channel* to, \
+			std::string const & message)
+{
+	std::vector<Client*>	clients = to->getClients();
+	for (Channel::clientlist_iterator_t it = clients.begin(); it != clients.end(); it++)
+		server->reply((*it)->getConnection(), message);
+}
